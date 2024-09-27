0c405efaae0ac7dbd0d7cd2cff1782decafb3a89
Örkényi István <istvanorkenyi300@gmail.com>
2024-09-27 12:30:03 +0200

    made the appointment schema

---
+++ butterflyhajszalon-backend/models/appointmentmodel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose")
+const {v4: uuidv4} = require('uuid')
+
+const appointmentSchema = mongoose.Schema({
+  foglalas_id: {
+    type: String,
+    default: uuidv4,
+    unique: true
+  },
+  user_name: {
+    type: String
+  },
+  user_email: {
+    type: String
+  },
+  user_tel: {
+    type: String
+  },
+  user_datestring: {
+    type: String
+  },
+  user_hour: {
+    type: String
+  },
+  user_service: {
+    type: String
+  }
+}, {
+  timestamps: true,
+}
+)
+module.exports = mongoose.model("Appointment", appointmentSchema)
